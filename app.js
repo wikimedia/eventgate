62f07fe317af2f4027ac9a1f19903a5a834d0509
Andrew Otto <acotto@gmail.com>
2019-06-18 11:46:27 -0400

    Allow configuration of bodyParser limit via app.conf.max_body_size
    Bug: T226026
    
    Cherry-Pick from
    https://github.com/ottomata/service-template-node/commit/182163a2fc1e6c99cc26d0e8fb90e1503c157087
    
    Conflicts:
    	config.dev.yaml
    	config.prod.yaml
    
    Change-Id: I9ad438068f7dbc4c1da0c6e0a9dfccccd438042e

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ function initApp(options) {
     // enable compression
     app.use(compression({ level: app.conf.compression_level }));
     // use the JSON body parser
-    app.use(bodyParser.json());
+    app.use(bodyParser.json({ limit: app.conf.max_body_size || '100kb' }));
     // use the application/x-www-form-urlencoded parser
     app.use(bodyParser.urlencoded({ extended: true }));
 
